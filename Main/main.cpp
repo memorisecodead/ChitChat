e2bab2215634297b4c5b9ffc428127228342c20d
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-09-10 18:47:49 +0300

    Have updated structure of pat

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,7 +1,28 @@
-#include "Network/Listener.hpp"
-#include "Network/SharedState.hpp"
+#include <boost/asio/signal_set.hpp>
 
-int main()
+#include <Network/Listener.hpp>
+#include <Network/SharedState.hpp>
+
+int main(int argc, char* argv[])
 {
+    std::string host{"127.0.0.1"};
+    uint16_t port = 8080;
+    std::string doc_root{ "." };
 
+    netAsio::io_context io;
+
+    std::make_shared<Listener>(
+        io, tcp::endpoint{netAsio::ip::make_address(host), port},
+        std::make_shared<Shared_state>(doc_root))->run();
+
+    netAsio::signal_set signals(io, SIGINT, SIGTERM);
+    signals.async_wait(
+        [&io](boost::system::error_code const&, int)
+        {
+            io.stop();
+        });
+
+    io.run();
+
+    return EXIT_SUCCESS;
 }
