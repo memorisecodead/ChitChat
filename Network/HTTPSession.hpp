076d801c5af3c0e9e58db2a112aeaaf6805338e9
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-09-10 21:42:02 +0300

    Have made Network lib

--- Network/HTTPSession.hpp
+++ Network/HTTPSession.hpp
@@ -7,24 +7,6 @@
 #include <cstdlib>
 #include <memory>
 
-class HTTPSession : public std::enable_shared_from_this<HTTPSession>
-{
-	tcp::socket _socket;
-	boost::beast::flat_buffer _buffer;
-	std::shared_ptr<Shared_state> _state;
-	boost::beast::http::request<boost::beast::http::string_body> _req;
-
-	void fail(error_code ec, const char* what);
-	void onRead(error_code ec, std::size_t);
-	void onWrite(error_code ec, std::size_t, bool close);
-
-public:
-	HTTPSession(tcp::socket socket, const std::shared_ptr<Shared_state>& state)
-		: _socket(std::move(socket)), 
-		  _state(state) {}
-
-	void run();
-};
 
 boost::beast::string_view mime_type(boost::beast::string_view path)
 {
@@ -174,6 +156,26 @@ void handle_request(
     res.keep_alive(req.keep_alive());
     return send(std::move(res));
 }
+
+
+class HTTPSession : public std::enable_shared_from_this<HTTPSession>
+{
+	tcp::socket _socket;
+	boost::beast::flat_buffer _buffer;
+	std::shared_ptr<Shared_state> _state;
+	boost::beast::http::request<boost::beast::http::string_body> _req;
+
+	void fail(error_code ec, const char* what);
+	void onRead(error_code ec, std::size_t);
+	void onWrite(error_code ec, std::size_t, bool close);
+
+public:
+	HTTPSession(tcp::socket socket, const std::shared_ptr<Shared_state>& state)
+		: _socket(std::move(socket)), 
+		  _state(state) {}
+
+	void run();
+};
 
 void HTTPSession::fail(error_code ec, const char* what)
 {
