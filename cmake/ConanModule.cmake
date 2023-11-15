a81af7d54b6dc3c9b988ea9ca76b1600d9ac1fdf
memorisecodead <e33lement.kotoff@gmail.com>
2023-11-15 04:31:12 +0300

    UPD

--- cmake/ConanModule.cmake
+++ cmake/ConanModule.cmake
@@ -15,10 +15,12 @@ conan_cmake_autodetect(settings)
 conan_cmake_install(PATH_OR_REFERENCE .
                     REMOTE conancenter
                     BUILD missing
-                    SETTINGS ${settings})
+                    INSTALL_FOLDER ${CMAKE_BINARY_DIR}/Conan
+                    SETTINGS ${settings}
+                    ENV "F77=no"
+                    ENV "FCs=no")
 
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/Conan)
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
 
 find_package(Boost REQUIRED)
 find_package(Catch2 REQUIRED)
