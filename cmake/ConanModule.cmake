93a334dd9371bbce8e200792f41cf98c329c43a3
memorisecodead <e33lement.kotoff@gmail.com>
2023-11-17 19:50:20 +0300

    chck

--- cmake/ConanModule.cmake
+++ cmake/ConanModule.cmake
@@ -7,8 +7,16 @@ endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-conan_cmake_run(REQUIRES boost/1.81.0
-						 catch2/3.4.0
-                BASIC_SETUP CMAKE_TARGETS
-                BUILD missing)
-				
+conan_cmake_configure(REQUIRES boost/1.81.0
+							   catch2/3.4.0 
+                      GENERATORS cmake_find_package) 
+ 
+conan_cmake_autodetect(settings) 
+ 
+conan_cmake_install(PATH_OR_REFERENCE . 
+                    BUILD missing 
+                    REMOTE conancenter 
+                    SETTINGS ${settings}) 
+
+find_package(Boost REQUIRED)
+find_package(Catch2 REQUIRED)
